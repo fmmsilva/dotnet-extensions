c040acc34953dfab609493a6eb81cca8ca9694eb
fmmsilva <fmmsilva@gmail.com>
2015-03-02 16:33:49 -0300

    métodos RemoveDiacritics e Slugify

---
+++ src/DotNetExtensions.Test/StringTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNetExtensions;
+
+namespace DotNetExtensions.Test
+{
+    [TestClass]
+    public class StringTest
+    {
+
+        [TestMethod]
+        public void RemoveDiacritics()
+        {
+            string result = "Mendonça".RemoveDiacritics();
+            Assert.AreEqual(result, "Mendonca");
+
+            result = "áÉíöû".RemoveDiacritics();
+            Assert.AreEqual(result, "aEiou");
+        }
+
+
+        [TestMethod]
+        public void Slugify()
+        {
+            string result = "Felipe Mendonça Marques Silva".Slugify();
+            Assert.AreEqual(result, "felipe-mendonca-marques-silva");
+
+            result = "Tom & Jerry".Slugify();
+            Assert.AreEqual(result, "tom-jerry");
+
+            result = " Teste com  2  espacos ".Slugify();
+            Assert.AreEqual(result, "teste-com-2-espacos");
+        }
+    }
+}
---
+++ src/DotNetExtensions/StringExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DotNetExtensions
+{
+    public static class StringExtensions
+    {
+        public static string Slugify(this string value)
+        {
+
+            //First to lower case
+            value = value.ToLowerInvariant();
+
+            //Remove all accents
+            var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(value);
+            value = Encoding.ASCII.GetString(bytes);
+
+            //Replace spaces
+            value = Regex.Replace(value, @"\s", "-", RegexOptions.Compiled);
+
+            //Remove invalid chars
+            value = Regex.Replace(value, @"[^a-z0-9\s-_]", "", RegexOptions.Compiled);
+
+            //Trim dashes from end
+            value = value.Trim('-', '_');
+
+            //Replace double occurences of - or _
+            value = Regex.Replace(value, @"([-_]){2,}", "$1", RegexOptions.Compiled);
+
+            return value;
+        }
+
+        public static string RemoveDiacritics(this string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
